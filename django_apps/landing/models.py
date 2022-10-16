7c4ad0b9673c775ac0b293ead0afc4b1de5ba915
abishekrv <abishekrvcodes@gmail.com>
2022-10-16 10:22:23 +0530

    Increased about section char field length
    - Now paragraphs in about section can accommodate upto 1200 chars

--- django_apps/landing/models.py
+++ django_apps/landing/models.py
@@ -7,7 +7,7 @@ class AboutMeContent(models.Model):
         db_table = 'about_me_content'
 
     paragraph_id = models.IntegerField(primary_key=True)
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1200)
 
     def __str__(self):
         return self.description[:20] + '...'
